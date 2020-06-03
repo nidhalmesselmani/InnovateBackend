e6810b97595387c8bf5d3b55cb8bdf0f1a9579fd
Nidhal.Messelmani <nidhalmesselmani@fisglobal.com>
2020-06-03 14:51:33 +0100

    deleted timestamp

--- app/User.php
+++ app/User.php
@@ -17,8 +17,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'id', 'username', 'first_name','last_name','date_of_birth','mail', 'phone','status','cin'
-    ];
-    
+    ];    
+    public $timestamps = false;
     /**
          * The attributes that should be hidden for arrays.
      */
